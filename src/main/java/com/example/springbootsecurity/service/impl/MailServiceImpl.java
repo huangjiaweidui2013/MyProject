00498865e3cefd79128c0926bed1f6b10f7f0aff
huanglang <1037084308@qq.com>
2021-11-16 10:04:52 +0800

    暂时屏蔽nacos配置和发送邮件配置,以使得项目可以启动

--- src/main/java/com/example/springbootsecurity/service/impl/MailServiceImpl.java
+++ src/main/java/com/example/springbootsecurity/service/impl/MailServiceImpl.java
@@ -1,18 +1,10 @@
 package com.example.springbootsecurity.service.impl;
 
-import cn.hutool.core.util.ArrayUtil;
 import com.example.springbootsecurity.service.MailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.io.File;
 
 /**
  * @projectName: SpringBootSecurity
@@ -25,10 +17,10 @@ import java.io.File;
 @RequiredArgsConstructor
 @Service
 public class MailServiceImpl implements MailService {
-    private final JavaMailSender mailSender;
+//    private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
-    private String from;
+//    @Value("${spring.mail.username}")
+//    private String from;
 
     /**
      * 发送文本邮件
@@ -40,15 +32,15 @@ public class MailServiceImpl implements MailService {
      */
     @Override
     public void sendSimpleMail(String to, String subject, String content, String... cc) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(content);
-        if (ArrayUtil.isNotEmpty(cc)) {
-            message.setCc(cc);
-        }
-        mailSender.send(message);
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom(from);
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(content);
+//        if (ArrayUtil.isNotEmpty(cc)) {
+//            message.setCc(cc);
+//        }
+//        mailSender.send(message);
     }
 
     /**
@@ -62,16 +54,16 @@ public class MailServiceImpl implements MailService {
      */
     @Override
     public void sendHtmlMail(String to, String subject, String content, String... cc) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setFrom(from);
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(content, true);
-        if (ArrayUtil.isNotEmpty(cc)) {
-            helper.setCc(cc);
-        }
-        mailSender.send(message);
+//        MimeMessage message = mailSender.createMimeMessage();
+//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//        helper.setFrom(from);
+//        helper.setTo(to);
+//        helper.setSubject(subject);
+//        helper.setText(content, true);
+//        if (ArrayUtil.isNotEmpty(cc)) {
+//            helper.setCc(cc);
+//        }
+//        mailSender.send(message);
     }
 
     /**
@@ -86,21 +78,21 @@ public class MailServiceImpl implements MailService {
      */
     @Override
     public void sendAttachmentsMail(String to, String subject, String content, String filePath, String... cc) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setFrom(from);
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(content, true);
-        if (ArrayUtil.isNotEmpty(cc)) {
-            helper.setCc(cc);
-        }
-        FileSystemResource file = new FileSystemResource(new File(filePath));
-        String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
-        helper.addAttachment(fileName, file);
-
-        mailSender.send(message);
+//        MimeMessage message = mailSender.createMimeMessage();
+//
+//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//        helper.setFrom(from);
+//        helper.setTo(to);
+//        helper.setSubject(subject);
+//        helper.setText(content, true);
+//        if (ArrayUtil.isNotEmpty(cc)) {
+//            helper.setCc(cc);
+//        }
+//        FileSystemResource file = new FileSystemResource(new File(filePath));
+//        String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
+//        helper.addAttachment(fileName, file);
+//
+//        mailSender.send(message);
     }
 
     /**
@@ -117,19 +109,19 @@ public class MailServiceImpl implements MailService {
     @Override
     public void sendResourceMail(String to, String subject, String content, String rscPath, String rscId,
                                  String... cc) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setFrom(from);
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(content, true);
-        if (ArrayUtil.isNotEmpty(cc)) {
-            helper.setCc(cc);
-        }
-        FileSystemResource res = new FileSystemResource(new File(rscPath));
-        helper.addInline(rscId, res);
-
-        mailSender.send(message);
+//        MimeMessage message = mailSender.createMimeMessage();
+//
+//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//        helper.setFrom(from);
+//        helper.setTo(to);
+//        helper.setSubject(subject);
+//        helper.setText(content, true);
+//        if (ArrayUtil.isNotEmpty(cc)) {
+//            helper.setCc(cc);
+//        }
+//        FileSystemResource res = new FileSystemResource(new File(rscPath));
+//        helper.addInline(rscId, res);
+//
+//        mailSender.send(message);
     }
 }
